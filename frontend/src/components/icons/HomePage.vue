682e289edc2d6df365da3843f2cbda616259bf09
penraiwahey <142884879+penraiwahey@users.noreply.github.com>
2025-08-12 23:59:59 +0700

    revision 4.1

--- frontend/src/components/icons/HomePage.vue
+++ frontend/src/components/icons/HomePage.vue
@@ -1,6 +1,9 @@
 <template>
-      <h1 class="text-4xl font-bold text-gray-800">Welcome to Pos Exemple!</h1>
+  <div>     
+     <h1 class="text-4xl font-bold text-gray-800">Welcome to Pos Exemple!</h1>
       <p class="mt-4 text-gray-600">
         This is the main content area of your e-commerce website.
       </p>
+    </div>
+
 </template>
